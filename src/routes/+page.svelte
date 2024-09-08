d8965f7e160d96ba3d80237c39a9509c6d17257e
Liande Guo <104030726+liandeguo@users.noreply.github.com>
2024-09-08 14:43:07 +0200

    new portfolio

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,87 +1,76 @@
-<!-- <div id="vim">
-    <p>-- INSERT --</p>
-    <p>1,2</p>
-    <p>All</p>
-</div> -->
-
-<script>
-    let options = [
-        {
-            name: "[01] More About Me",
-            link: "aboutme",
-        },
-        {
-            name: "[02] My Skills",
-            link: "myskills",
-        },
-        {
-            name: "[03] My Work",
-            link: "mywork",
-        },
-        {
-            name: "[04] Personal Projects",
-            link: "mywork",
-        },
-        {
-            name: "[05] Contact & Social Media",
-            link: "contact",
-        },
-    ];
-    let selected = -1;
-    function onKeyDown(e) {
-        switch (e.keyCode) {
-            case 40:
-                if (selected >= -1) {
-                    selected = (selected + 1) % options.length;
-                }
-                break;
-            case 74:
-                if (selected >= -1) {
-                    selected = (selected + 1) % options.length;
-                }
-                break;
-
-            case 38:
-                if (selected > 0) {
-                    selected = (selected - 1) % options.length;
-                }
-                break;
-            case 75:
-                if (selected > 0) {
-                    selected = (selected - 1) % options.length;
-                }
-                break;
-            case 13:
-                if (selected == 0) {
-                    window.location = "aboutme";
-                }
-                if (selected == 1) {
-                    window.location = "myskills";
-                }
-                if (selected == 2) {
-                    window.location = "mywork";
-                }
-                if (selected == 3) {
-                    window.location = "personalprojects";
-                }
-                if (selected == 4) {
-                    window.location = "contact";
-                }
-        }
-    }
-</script>
-
-<div style="">
-    <h1>&lt;Liande Guo/&gt;</h1>
-    <pre>
-        Hi! I'm Liande a passionate Frontend Developer from Germany 
-
-    </pre>
-    {#each options as option, index}
-        <p class="selectable" class:selectedH={index == selected}>
-            <a id="select_a" href={option.link}>{option.name}</a>
+<main>
+    <div>
+        <div class="hero"></div>
+        <h1>Hey! I'm Leander.</h1>
+        <hr />
+        <p>i am a 14 year old programmer from germany.</p>
+        <br />
+        <p>
+            i like to do <h>frontend development</h> and <h>graphics design</h> and
+            am currently a student @ KWR
         </p>
-    {/each}
-</div>
-
-<svelte:window on:keydown|preventDefault={onKeyDown} />
+        <br />
+        <p>
+            if you would like to contact me or have a look at my profiles here
+            are some options
+        </p>
+        <div style="" class="contact">
+            <a href="https://github.com/liandeguo"
+                ><img src="/icons/github.svg" alt="" /> @liandeguo</a
+            >
+            <a href="https://www.linkedin.com/in/leanderguo/"
+                ><img src="/icons/linkedin.svg" alt="" /> @leanderguo</a
+            >
+            <a href="https://dribbble.com/liandeguo"
+                ><img src="/icons/dribbble.svg" alt="" /> @liandeguo</a
+            >
+            <a href="https://www.instagram.com/liande.g/"
+                ><img src="/icons/instagram.svg" alt="" /> @liande.g</a
+            >
+        </div>
+        <br />
+        <br />
+        <div class="projects">
+            <h2>my projects</h2>
+            <br />
+            <div class="project" id="soty">
+                <h3>Soty Ltd.</h3>
+                <p>
+                    i made the website for soty ltd. in <br />
+                    <h>SvelteKit HTML CSS</h>
+                    and <h>JavaScript</h>
+                </p>
+                <div class="links">
+                    <a href="https://github.com/Soty-Ltd/website-new"
+                        ><img src="/icons/github.svg" color="white" alt="" /></a
+                    >
+                    <a href="https://soty.one"
+                        ><img src="/icons/link.svg" alt="" /></a
+                    >
+                </div>
+            </div>
+            <hr />
+            <div class="project" id="soty">
+                <h3>Brawl Stars Tracker</h3>
+                <p>
+                    i made a website that can track your progress in the game <br
+                    />
+                    called brawlstars using their
+                    <h>api SvelteKit HTML JavaScript</h>
+                    and <h>CSS</h>
+                </p>
+                <div class="links">
+                    <a href="https://github.com/liandeguo/brawlstats"
+                        ><img src="/icons/github.svg" alt="" /></a
+                    >
+                    <a href="https://brawlstats.vercel.app/"
+                        ><img src="/icons/link.svg" alt="" /></a
+                    >
+                </div>
+            </div>
+            <hr />
+        </div>
+        <br />
+        <br />
+    </div>
+</main>
